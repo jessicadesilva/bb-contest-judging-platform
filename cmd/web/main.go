81839c25cb494b1575ff5e5059cc6a2d9d86b7fe
jessicadesilva <jdesilva1@csustan.edu>
2024-07-17 19:15:53 -0700

    Enable HTTP network address config

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	// Command line flag for network address with default value :4000.
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	// Parse command line flag value and assign to addr variable.
+	flag.Parse()
+
 	// Use the http.NewServeMux() function to initialize a new servemux.
 	mux := http.NewServeMux()
 
@@ -22,11 +28,12 @@ func main() {
 	mux.HandleFunc("GET /results/{contest}/{year}/{division}/{class}", classResults)
 
 	// Print a log message to say that the server is starting.
-	log.Print("starting server on :4000")
+	// Dereference the pointer addr to the flag value.
+	log.Printf("starting server on %s", *addr)
 
 	// Use the http.ListenAndServe() function to start a new web server.
 	// If http.ListenAndServe() returns an error we use the log.Fatal()
 	// function to log the error message and exit.
-	err := http.ListenAndServe(":4000", mux)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
