5067a1f5481d82ff38c7f09e429c82d9bde5e4e5
jessicadesilva <jdesilva1@csustan.edu>
2024-07-19 16:57:48 -0700

    Add a Get method to CompetitorModel.

--- internal/models/competitor.go
+++ internal/models/competitor.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"database/sql"
+	"errors"
 )
 
 // Define a Competitor type to hold the data for an individual competitor.
@@ -39,4 +40,37 @@ func (m *CompetitorModel) Insert(Name string, Location string) (int, error) {
 
 	// Convert ID from int64 to int type before returning.
 	return int(id), nil
+}
+
+func (m *CompetitorModel) Get(CompetitorID int) (Competitor, error) {
+	// SQL statement to execute.
+	stmt := `SELECT * FROM judgingplatform.competitors
+		WHERE id = ?`
+
+	// Execute the SQL statement using the QueryRow() method
+	// on the connection pool. This returns a pointer to a sql.Row
+	// object which holds the result from the database.
+	row := m.DB.QueryRow(stmt, CompetitorID)
+
+	// Initialize a new zeroed Competitor struct.
+	var c Competitor
+
+	// Copy the values from each field in sql.Row to
+	// the corresponding filed in the Competitor struct
+	// using row.Scan().
+	err := row.Scan(&c.ID, &c.Name, &c.Location)
+
+	if err != nil {
+		// If the query returns no rows, we get a sql.ErrNoRows
+		// error wich we can check for using errors.Is() to return
+		// a ErrNoRecord error instead.
+		if errors.Is(err, sql.ErrNoRows) {
+			return Competitor{}, ErrNoRecord
+		} else {
+			return Competitor{}, err
+		}
+	}
+
+	// If everything went OK, return the filled Competitor struct.
+	return c, nil
 }
